8c4e39c00051ec8ce053854f77c4f2516a175bae
Peter Indiola <pindiola@gmail.com>
2012-08-02 01:35:01 +0800

    Added sandbox url and fix jsonp call

--- includes/json.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	$data = array();
-	$data['service_id'] = '123456789';
-	$data['email'] = $_GET['email'];
-	echo $_GET['jsoncallback'] . '('. json_encode($data). ')';
- ?>
--- js/bf_admin.js
+++ js/bf_admin.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
 		e.preventDefault();
 		var email = $(this).find( 'input[name="email"]').val();
 		var password = $(this).find( 'input[name="password"]').val();
-		var url = jsonp.url + '?jsoncallback=?';
+		var url = 'http://peter.sandbox.boofresh.com/cindex.php/account/ajax?callback=?'; 
 		var service_id;
 
 		if(email.length == 0 || password.length == 0) {
@@ -14,32 +14,28 @@ jQuery(document).ready(function($) {
 				$(this).css('color', '#ffffff');
 	        });
 		} else {
-			$.getJSON(url, { email: email, password: password }, function(data){
-				service_id = data.service_id;
-				if(service_id) {
-					var data = {
-						action: 'bf_member',
-						service_id: service_id, 
-						email: email, 
-						password: password,
-						bf_nonce: bf_nonce.nonce
-					};
-					$.post(ajax_script.ajaxurl, data, function(response){
-						$("#bf_flash_message").show().fadeIn('slow', function(){
-							$(this).empty();
-							$(this).css('background-color', '#FFFBCC');
-							$(this).css('color', '#000');
-							$(this).append('Account Info Saved!');
-							$("#service_id").empty().append('<b>BookFresh Service ID:</b> ' + data.service_id);
-							$("#bf_email").empty().append('<b>BookFresh Email:</b> ' + data.email);
+			$.getJSON(url, { xaction: 'login', user_name: email, password: password }, function(service){
+				var data = {
+					action: 'bf_member',
+					service_id: service.service_id, 
+					email: email, 
+					password: password,
+					bf_nonce: bf_nonce.nonce
+				};
+				$.post(ajax_script.ajaxurl, data, function(response){
+					$("#bf_flash_message").show().fadeIn('slow', function(){
+						$(this).empty();
+						$(this).css('background-color', '#FFFBCC');
+						$(this).css('color', '#000');
+						$(this).append('Account Info Saved!');
+						$("#service_id").empty().append('<b>BookFresh Service ID:</b> ' + data.service_id);
+						$("#bf_email").empty().append('<b>BookFresh Email:</b> ' + data.email);
 
-							$(this).fadeOut(3000);
-				        });
-				    });
-				}
+						$(this).fadeOut(3000);
+			        });
+			    });
 			});
+			return false;
 		}
-		return false;
-		
 	});
 });
