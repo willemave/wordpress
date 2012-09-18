a8868d2993ddccd08903cf5e626cbfd60b0f7ac2
Willem Ave <willem.ave@gmail.com>
2012-09-17 17:21:33 -0700

    added a step 4 for installation instructions

--- includes/class-pluginmethods.php
+++ includes/class-pluginmethods.php
@@ -107,6 +107,7 @@ if (!class_exists('BF_PluginMethods')) {
 							<li><span>Step 1:</span> <a href="http://www.bookfresh.com/pricing/" target="_blank">Signup with Bookfresh!</a></li>
 							<li><span>Step 2:</span> Type your Email and Password into the WordPress Plugin to the left</li>
 							<li><span>Step 3:</span> Pick and use a ShortCode anywhere in your Site!</li>
+							<li><span>Step 4:</span> Log in to <a href="http://www.bookfresh.com/index.html?view=login" target="_blank">Bookfresh</a> and customize the look and feel of your widget!</li>
 						</ul>
 						<div id="widget_large" class="widgets_info">
 							<span>[bookfresh_widget_large]</span>
